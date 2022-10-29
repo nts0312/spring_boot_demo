0d22dbe18be9227a8518010f144f534d36a546f5
Nitin Singh <n_t_z@Nitins-MacBook-Air.local>
2022-10-30 00:31:19 +0530

    added a mapping to fetch all users and userID wise

--- src/main/java/com/example/demo/restapi/user/UserDaoService.java
+++ src/main/java/com/example/demo/restapi/user/UserDaoService.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 
 @Component
@@ -26,11 +27,18 @@ public class UserDaoService {
 	
 	static {
 		users.add(new User(1, "Nitin", LocalDate.now().minusYears(25) ));
-		users.add(new User(1, "Harry", LocalDate.now().minusYears(24) ));
-		users.add(new User(1, "Prisha", LocalDate.now().minusYears(27) ));
+		users.add(new User(2, "Harry", LocalDate.now().minusYears(24) ));
+		users.add(new User(3, "Prisha", LocalDate.now().minusYears(27) ));
 	}
 	
 	public List<User> findAll(){
 		return users;
+	}
+	
+	public User findOne(int id){
+		Predicate<? super User> predicate = user -> user.getId().equals(id);
+		
+		
+		return users.stream().filter(predicate).findFirst().get();
 	}
 }
